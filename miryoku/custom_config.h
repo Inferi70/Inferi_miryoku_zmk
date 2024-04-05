3ffac150121443b9cdb5d879a3811006ee57a44c
Inferi70 <92383434+Inferi70@users.noreply.github.com>
2024-04-05 01:56:35 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -31,17 +31,17 @@ U_NA, U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,     
 &kp B, &kp LCTRL, &kp Z, &kp X, &kp C, &kp V,   &kp N, &kp M,  &kp COMMA, &kp DOT, &kp FSLH, &kp ESC, \
                   &kp LALT, &kp LCTRL, &kp SPACE,   &kp RET, &mo 1, &mo 2
 
-// // Direct custom layer access
-// #define MIRYOKU_LAYOUTMAPPING_CORNE( \
-// K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
-// K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
-// K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
-// N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
-// ) \
-// XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-// XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-// XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-//                K32  K33  K34       K35  K36  K37
+// Direct custom layer access
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+K00, K01, K02, K03, K04, K05,                    K06, K07, K08, K09, K10, K11, \
+K12, K13, K14, K15, K16, K17,                    K18, K19, K20, K21, K22, K23, \
+K24, K25, K26, K27, K28, K29,                    K30, K31, K32, K33, K34, K35, \
+               K36, K37, K38,                    K39, K40, K41 \
+) \
+K00, K01, K02, K03, K04, K05,                    K06, K07, K08, K09, K10, K11, \
+K12, K13, K14, K15, K16, K17,                    K18, K19, K20, K21, K22, K23, \
+K24, K25, K26, K27, K28, K29,                    K30, K31, K32, K33, K34, K35, \
+               K36, K37, K38,                    K39, K40, K41 
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
