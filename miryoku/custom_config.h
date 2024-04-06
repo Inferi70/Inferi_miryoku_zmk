dbce3ab35a168294dad9e8e1124b2edd9762b3dc
Inferi70 <92383434+Inferi70@users.noreply.github.com>
2024-04-05 22:40:35 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -41,28 +41,29 @@ U_NP,              U_NP,              &kp LALT,          &kp LCTRL,         &kp 
 // %kp B   &kp LCTRL  &kp Z   &kp X      &kp C      &kp V                 K25  K26  K27  K28  K29  &kp ESC \
 //                            &kp LALT   &kp LCTRL  &kp SPACE             K35  K36  K37
 
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
-// #define MIRYOKU_LAYER_LIST \
-// MIRYOKU_X(BASE,   "Base") \
-// MIRYOKU_X(EXTRA,  "Extra") \
-// MIRYOKU_X(TAP,    "Tap") \
-// MIRYOKU_X(BUTTON, "Button") \
-// MIRYOKU_X(NAV,    "Nav") \
-// MIRYOKU_X(MOUSE,  "Mouse") \
-// MIRYOKU_X(MEDIA,  "Media") \
-// MIRYOKU_X(NUM,    "Num") \
-// MIRYOKU_X(SYM,    "Sym") \
-// MIRYOKU_X(FUN,    "Fun") \
-// MIRYOKU_X(GAME,   "Game")
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GAME,   "Game")
 
-// #define U_BASE   0
-// #define U_EXTRA  1
-// #define U_TAP    2
-// #define U_BUTTON 3
-// #define U_NAV    4
-// #define U_MOUSE  5
-// #define U_MEDIA  6
-// #define U_NUM    7
-// #define U_SYM    8
-// #define U_FUN    9
-// #define U_GAME   10
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_GAME   10
