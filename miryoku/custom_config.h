576895571be178dfc7be6a1974852e65ef1c5168
Inferi70 <92383434+Inferi70@users.noreply.github.com>
2024-04-05 21:50:37 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#if defined (KEYBOARD_CRKBD)
-
 #define XXX &none
 
 #define MIRYOKU_LAYERMAPPING_BASE(\
@@ -15,5 +12,3 @@
 &kp LSHFT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT  \
 &kp LCTRL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp ESC  \
                      K32  K33  K34       K35  K36  K37
-
-#endif
