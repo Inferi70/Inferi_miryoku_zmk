804e4c7dd858d927456ca7c2db436010e4905c6f
Inferi70 <92383434+Inferi70@users.noreply.github.com>
2024-04-05 23:44:53 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -30,16 +30,18 @@ XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                 K35  K36  K37
 
-#define MIRYOKU_LAYERMAPPING_GAME( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-&kp ESC &kp TAB    &kp Q    &kp W      &kp N3     &kp N4              K05  K06  K07  K08  K09  XXX \
-&kp G   &kp LSHFT  &kp A     &kp S      &kp D      &kp F               K15  K16  K17  K18  K19  XXX \
-&kp B   &kp LCTRL  &kp Z     &kp X      &kp C      &kp V               K25  K26  K27  K28  K29  XXX \
-                             &kp LALT   &kp LCTRL  &kp SPACE           K35  K36  K37
+// #define MIRYOKU_LAYERMAPPING_GAME( \
+//      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+//      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+//      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+//      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+// ) \
+// &kp ESC &kp TAB    &kp Q    &kp W      &kp N3     &kp N4              K05  K06  K07  K08  K09  XXX \
+// &kp G   &kp LSHFT  &kp A     &kp S      &kp D      &kp F               K15  K16  K17  K18  K19  XXX \
+// &kp B   &kp LCTRL  &kp Z     &kp X      &kp C      &kp V               K25  K26  K27  K28  K29  XXX \
+//                              &kp LALT   &kp LCTRL  &kp SPACE           K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
