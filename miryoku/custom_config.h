0b092aaeb77a753be6322e099279d8f446722b73
Inferi70 <92383434+Inferi70@users.noreply.github.com>
2024-04-06 00:54:18 -0600

    workin(should) config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,17 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/ {
-     behaviors {
-                     m_td0: tap_dance_mod_tap_gaming_num1_2 {
-                     compatible = "zmk,behavior-tap-dance";
-                     #binding-cells = <0>;
-                     tapping-term-ms = <225>;
-                     bindings = <&mt N1 Q>, <&kp N2>;
-                     };
-             };
-};
-
 #define MIRYOKU_LAYER_GAME \
 &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SEMI,          \
